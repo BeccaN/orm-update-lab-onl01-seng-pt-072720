8d6eb5adda11f60221a03a5a648bfaaad5d20efb
rncongi <rncongi@gmail.com>
2020-10-01 20:30:47 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -29,15 +29,18 @@ class Student
   end 
 
   def save
-    sql = <<-SQL
-    INSERT INTO students(name, grade)
-    VALUES(?, ?)
-    SQL
+    if self.id
+      self.update
+    else
+      sql = <<-SQL 
+        INSERT INTO students (name, grade)
+        VALUES (?,?)
+      SQL
 
-    DB[:conn].execute(sql, self.name, self.grade)
-
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
-  end 
+      DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
+  end
 
   def self.create(name, grade)
     student = Student.new(name, grade)
@@ -46,11 +49,10 @@ class Student
   end 
 
   def self.new_from_db(row)
-    new_student = self.new
-    new_student.id = row[0]
-    new_student.name = row[1]
-    new_student.grade = row[2]
-    new_student
+    id = row[0]
+    name = row[1]
+    grade = row[2]
+    self.new(id, name, grade)
   end 
 
   def self.find_by_name(name)
@@ -58,7 +60,6 @@ class Student
       SELECT *
       FROM students
       WHERE name = ?
-      LIMIT 1
     SQL
 
     DB[:conn].execute(sql,name).map do |row|
@@ -67,7 +68,8 @@ class Student
   end 
 
   def update
-    sql = "UPDATE songs SET name = ?, album = ? WHERE id = ?"
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
     DB[:conn].execute(sql, self.name, self.grade, self.id)
   end 
-end
+
+end 
